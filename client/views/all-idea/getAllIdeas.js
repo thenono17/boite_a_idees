fed854fabc7fa3ee961228542ca87fb0afb4509e
martin-samson-png <martinsams64@gmail.com>
2025-08-08 13:34:33 +0200

    aaaaa

--- client/views/all-idea/getAllIdeas.js
+++ client/views/all-idea/getAllIdeas.js
@@ -17,6 +17,29 @@ updateUserLink();
 
     const ideas = await res.json();
     console.log(ideas);
+
+    const ideasContainer = document.getElementById("IdeasContainer");
+    const template = document.getElementById("CardIdeaTemplate");
+
+    ideas.forEach((idea) => {
+      const card = template.content.cloneNode(true);
+
+      const cardRoot = card.querySelector(".col-12");
+
+      cardRoot.setAttribute("data-id", idea.idea_id);
+
+      card.querySelector("#TilteIdea").textContent = idea.title || "Titre";
+      card.querySelector("#DescriptionIdea").textContent =
+        idea.descr || "Description";
+      card.querySelector("#NumberLikeIdea").textContent = idea.likes || 0;
+
+      const link = card.querySelector("a");
+      if (idea.id) {
+        link.href = `./../idea/ideapage.html?id=${idea.id}`;
+      }
+
+      ideasContainer.appendChild(card);
+    });
   } catch (err) {
     console.error("Erreur réseau :", err);
   }
